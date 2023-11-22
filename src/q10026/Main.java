f6af2e129fdf167c943129a0f4f399e6fcdc5f01
Park-GiJun <144126780+Park-GiJun@users.noreply.github.com>
2023-11-22 16:26:35 +0900

    10026

--- src/q10026/Main.java
+++ src/q10026/Main.java
@@ -5,21 +5,36 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    static int n;
-    static String[][] rgb;
-    static String[][] redGreen;
-    static boolean[][] visited;
+    static int n; // n*n 배열
+    static String[][] rgb; // 그림의 RGB위치
+    static String[][] rgb2; // 그림의 RGB위치
+    static boolean[][] visited; // 방문 여부
+    static boolean[][] visited2; // 방문 여부
 
-    static void dfs(int a, int b) {
-        if (a == 0 || b == 0 || a > n || b > n || visited[a][b]) {
+    static void dfs(int a, int b, String color) {
+        if (a < 0 || a >= n || b < 0 || b >= n || visited[a][b] || !rgb[a][b].equals(color)) {
             return;
         }
+
         visited[a][b] = true;
 
-        dfs(a + 1, b);
-        dfs(a - 1, b);
-        dfs(a, b + 1);
-        dfs(a, b - 1);
+        dfs(a + 1, b, color);
+        dfs(a - 1, b, color);
+        dfs(a, b + 1, color);
+        dfs(a, b - 1, color);
+    }
+
+    static void dfs2(int a, int b, String color) {
+        if (a < 0 || a >= n || b < 0 || b >= n || visited2[a][b] || !rgb2[a][b].equals(color)) {
+            return;
+        }
+
+        visited2[a][b] = true;
+
+        dfs2(a + 1, b, color);
+        dfs2(a - 1, b, color);
+        dfs2(a, b + 1, color);
+        dfs2(a, b - 1, color);
     }
 
     public static void main(String[] args) throws Exception {
@@ -29,17 +44,37 @@ public class Main {
         n = Integer.parseInt(br.readLine());
 
         rgb = new String[n][n];
-        redGreen = new String[n][n];
+        rgb2=new String[n][n];
         visited = new boolean[n][n];
+        visited2 = new boolean[n][n];
 
         for (int i = 0; i < n; i++) {
-            String inp = br.readLine();
+            String inpRGB = br.readLine();
             for (int j = 0; j < n; j++) {
-                rgb[i][j] = String.valueOf(inp.charAt(j));
-                visited[i][j] = false;
+                char char1 = inpRGB.charAt(j);
+                rgb[i][j] = String.valueOf(char1);
+                if (char1 == 'R' || char1 =='G') {
+                    char1 = 'A';
+                }
+                rgb2[i][j] = String.valueOf(char1);
             }
         }
 
+        int count = 0;
+        int count2 = 0;
 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visited[i][j]) {
+                    dfs(i, j, rgb[i][j]);
+                    count++;
+                }
+                if (!visited2[i][j]) {
+                    dfs2(i, j, rgb2[i][j]);
+                    count2++;
+                }
+            }
+        }
+        System.out.println(count + " " + count2);
     }
 }
