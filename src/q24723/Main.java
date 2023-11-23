787419c1164d4b745cf820e7a23f53847d743caf
Park-GiJun <144126780+Park-GiJun@users.noreply.github.com>
2023-11-23 16:56:43 +0900

    15439

---
+++ src/q24723/Main.java
@@ -0,0 +1,13 @@
+package q24723;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result = (int) Math.pow(2, n);
+        System.out.println(result);
+
+    }
+}
