2aef33c37549c6d629f5ff096d41de791ce93c18
Park-GiJun <144126780+Park-GiJun@users.noreply.github.com>
2023-11-28 09:29:15 +0900

    10039

---
+++ src/q9663/Main.java
@@ -0,0 +1,25 @@
+package q9663;
+
+import java.io.*;
+
+public class Main {
+
+    static int n;
+    static int count;
+    static int[][] chessBoard;
+    static boolean[][] conqueredByQueen;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        n = Integer.parseInt(br.readLine());
+        count = 0;
+
+        chessBoard = new int[n][n];
+        conqueredByQueen = new boolean[n][n];
+
+
+    }
+}
